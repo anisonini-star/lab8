078d7c23e9de471bf8f12373042d53b916c4e95f
anisonini-star <anisonini@gmail.com>
2026-06-17 19:59:59 +0300

    Create users.php

---
+++ users.php
@@ -0,0 +1,17 @@
+<?php
+// Массив пользователей с дополнительной информацией (email)
+$users = array(
+    "admin" => array(
+        "pwd" => "123456",
+        "email" => "admin@example.com"
+    ),
+    "user" => array(
+        "pwd" => "qwerty",
+        "email" => "user@example.com"
+    ),
+    "test" => array(
+        "pwd" => "test123",
+        "email" => "test@example.com"
+    )
+);
+?>
