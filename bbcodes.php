ebe36265c4df23c6d2671295725287669159f6f9
anisonini-star <anisonini@gmail.com>
2026-06-17 20:18:46 +0300

    Update bbcodes.php

--- bbcodes.php
+++ bbcodes.php
@@ -1,15 +1,4 @@
 <?php
-// Функция обрабатывающая bb коды
-// Поддерживает преобразование следующих bb кодов в html теги
-//[p] [/p] => <p> </p>
-//[b] [/b] => <b> </b>
-//[i] [/i] => <em> </em>
-//[u] [/u] => <u> </u>
-//[img]url[/img] => <img src = url> 
-//[url=url]текст[/url] => <a href = url>текст</a> 
-//[url]url[/url] => <a href = url> url </a> 
-//[br] => <br>
-//[bl] => 5 пробелов (красная строка)
 function parse_bb_codes($var) {
       $search = array(
                 '/\[p\](.*?)\[\/p\]/is',
@@ -19,8 +8,8 @@ function parse_bb_codes($var) {
                 '/\[img\](.*?)\[\/img\]/is',
                 '/\[url\](.*?)\[\/url\]/is',
                 '/\[url\=(.*?)\](.*?)\[\/url\]/is',
-                '/\[br\]/is',
-                '/\[bl\]/is'  // Добавлен новый bb-код для красной строки
+                '/\[br\]/',
+                '/\[bl\]/'  // новый bb-код для красной строки
                 );
 
       $replace = array(
@@ -32,7 +21,7 @@ function parse_bb_codes($var) {
                 '<a href="$1">$1</a>',
                 '<a href="$1">$2</a>',
                 '<br>',
-                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'  // 5 неразрывных пробелов
+                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' // 5 неразрывных пробелов
                 );
 
       $var = preg_replace ($search, $replace, $var);
