03d2a6a22edd38535465369c38f2f5ca8f46e861
anisonini-star <anisonini@gmail.com>
2026-06-17 20:15:37 +0300

    Update autorization.php

--- autorization.php
+++ autorization.php
@@ -1,7 +1,7 @@
 <?php if($_SESSION["autorized"] != true) { ?>
    <h2>Авторизация</h2>
    <form method="post">
-   Пользователь: 
+   Логин: 
    <div align="right"><input name="login" type="text"></div>  
    Пароль: 
    <div align="right"><input name="pwd" type="password"></div>
@@ -14,8 +14,19 @@
 <p align = "center">
 <?php echo $_SESSION["user"]; ?>
 </p>
-<p align = "center">
-Email: <?php echo $_SESSION["email"]; ?>
+<!-- Вывод дополнительной информации о пользователе -->
+<p align="center">
+<?php 
+// Получаем данные о пользователе из сессии
+$user_login = $_SESSION["user"];
+// Для получения полных данных нужен доступ к массиву users
+// Включаем файл users.php, если еще не включен
+include_once "users.php";
+if(isset($users[$user_login])) {
+    echo "Email: " . $users[$user_login]["email"] . "<br>";
+    echo "Полное имя: " . $users[$user_login]["fullname"];
+}
+?>
 </p>
 <div align="center"><a href="index.php?action=exit">Выйти</a></div>
    <?php } ?>
